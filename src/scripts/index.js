dfd48165918181c358702094f7fab858fc592c4f
Pedropaa <pedropaa2@gmail.com>
2023-01-01 13:29:44 -0300

    Entrega Finalizada

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,38 @@
-/* Monte sua lógica aqui */
+
+function render(array) {
+    const ul = document.querySelector("ul")
+    const h3 = document.querySelector("h3")
+
+    ul.innerHTML = ""
+    h3.innerHTML = ""
+
+    array.results.forEach(element => {
+        const li = document.createElement("li")
+        const img = document.createElement("img")
+        const p = document.createElement("p")
+        img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${element.url.slice(34, -1)}.png`
+
+        p.innerText = element.name[0].toUpperCase() + element.name.slice(1)
+
+        li.appendChild(img)
+        li.appendChild(p)
+        ul.appendChild(li)
+    });
+}
+
+function renderSearch(name) {
+    const ul = document.querySelector("ul")
+    ul.innerHTML = ""
+    const li = document.createElement("li")
+    const img = document.createElement("img")
+    const p = document.createElement("p")
+    img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${name.id}.png`
+
+    p.innerText = name.name[0].toUpperCase() + name.name.slice(1)
+
+    li.appendChild(img)
+    li.appendChild(p)
+    ul.appendChild(li)
+}
+
+
--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -1,1 +1,51 @@
-/* Monte a lógica das requisições aqui */
+async function getAllPokemons() {
+    const pokemons = await fetch(`https://pokeapi.co/api/v2/pokemon`, {
+        method: `GET`,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then(res => res.json())
+        .then(res => {
+            render(res)
+            return res
+        })
+    return pokemons
+}
+
+async function getPokemonByName(pokemonName) {
+    try {
+        const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`, {
+            method: `GET`,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(res => res.json())
+            .then(res => {
+                const h3 = document.querySelector("h3")
+                h3.innerHTML = ""
+                renderSearch(res)
+                return res
+
+            })
+        return pokemon
+    } catch {
+        getAllPokemons()
+        alert("Pokemon não existe")
+    }
+}
+function adicionaEvento() {
+    const search = document.querySelector(".search")
+    const input = document.querySelector("input")
+    const h3 = document.querySelector("h3")
+    const ul = document.querySelector("ul")
+    search.addEventListener("click", () => {
+        getPokemonByName(input.value.toLocaleLowerCase())
+        ul.innerHTML = ""
+        h3.innerText = "Carregando..."
+    })
+}
+
+getAllPokemons()
+adicionaEvento()
